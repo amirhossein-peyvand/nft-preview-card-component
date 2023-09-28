67431da821a98776b774df70dfbb725a6991e51c
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-09-28 15:34:02 -0700

    Applying some styles to the component

--- README.md
+++ README.md
@@ -25,3 +25,28 @@ If you are developing a production application, we recommend updating the config
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+<!-- ================================ -->
+
+### Primary
+
+- Soft blue: hsl(215, 51%, 70%)
+- Cyan: hsl(178, 100%, 50%)
+
+### Neutral
+
+- Very dark blue (main BG): hsl(217, 54%, 11%)
+- Very dark blue (card BG): hsl(216, 50%, 16%)
+- Very dark blue (line): hsl(215, 32%, 27%)
+- White: hsl(0, 0%, 100%)
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 18px
+
+### Font
+
+- Family: [Outfit](https://fonts.google.com/specimen/Outfit)
+- Weights: 300, 400, 600
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,16 +1,42 @@
+import "./sass/Card.scss";
 import equilibrium from "./assets/image-equilibrium.jpg";
+import ethereum from "./assets/icon-ethereum.svg";
+import clock from "./assets/icon-clock.svg";
+import avatar from "./assets/image-avatar.png";
 
 const Card = () => {
   return (
-    <div className="card">
+    <section className="card">
       <div className="imageContainer">
         <img src={equilibrium} alt="equilibrium" />
+        <div className="overlay"></div>
       </div>
       <div className="contentContainer">
-        <a href="#">Equilibrium #3429</a>
-        <p> Our Equilibrium collection promotes balance and calm.</p>
+        <a href="#" className="title">
+          Equilibrium #3429
+        </a>
+        <p className="desc">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+        <div className="info">
+          <span>
+            <img src={ethereum} alt="ethereum" />
+            <p>0.041 ETH</p>
+          </span>
+          <span>
+            <img src={clock} alt="clock" />
+            <p>3 days left</p>
+          </span>
+        </div>
+        <hr />
+        <div className="prof">
+          <img src={avatar} alt="avatar" />
+          <p>
+            Creation of <a href="#">Jules Wyvern</a>
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
